4a5d410d29c348308ad014ec7097aa80163e3693
saradhapriyavajraglobal <saradha.priya@vajraglobal.com>
2022-12-15 18:33:44 +0530

    priya

--- template-home.php
+++ template-home.php
@@ -8,7 +8,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
 
-<?php echo do_shortcode('[home_banner /]');?>
+<h1>Title</h1>
 
 <?php endwhile;
 
