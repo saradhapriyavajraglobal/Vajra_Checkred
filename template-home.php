02a859068cb5a79354331aa4a0ca9d72940472b7
bharathivajra <bharathi.n@vajraglobal.com>
2022-12-27 10:04:09 +0530

    slider current

--- template-home.php
+++ template-home.php
@@ -138,6 +138,45 @@ endif;
 </section>
 
 
+<section class="integration-sec">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-12">
+              <div class="integration">
+                  <div class="slide-inte">
+                      <div class="inte-carousel">
+                        <img src="/wp-content/uploads/2022/12/Logo-1.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-429.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-427.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-423.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-421.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-420.svg">
+                        <img src="/wp-content/uploads/2022/12/Group-429.svg"/>
+                      </div>
+                  </div>
+            </div>
+
+            <div class="integration">
+                    <div class="slide-inte">
+                      <div class="inte-carousel-rtl" dir="rtl">
+                        <img src="/wp-content/uploads/2022/12/Group-431.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-430.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-426.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-425.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-422.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-424.svg"/>
+                        <img src="/wp-content/uploads/2022/12/Group-419.svg"/>
+                      </div>
+                    </div>
+            </div>
+
+          </div>
+      </div>
+      
+    </div>
+    
+</section>
+
 <?php endwhile;
 
 get_footer();   ?>
