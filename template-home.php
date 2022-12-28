5d8798de8d474e2f15f7740df4a07ce4a93d4302
saradhapriyavajraglobal <saradha.priya@vajraglobal.com>
2022-12-28 09:29:53 +0530

    tab section , footer

--- template-home.php
+++ template-home.php
@@ -122,123 +122,207 @@ endif;
 ?>
 
 
-
+<?php if( have_rows('cloud_platform_tab') ):
+while ( have_rows('cloud_platform_tab') ) : the_row();?>
 <section class="cloud-tab-section">
 	<div class="container-fluid">
-		<h3 class="text-center main-tagline v-title-wwr">Enhance your <span class="v-tagline-r">cloud and SaaS security</span> </br> with the CheckRed platform</h3>
+		<?php if(get_sub_field('main_title')): ?>
+		<h3 class="text-center main-tagline v-title-wwr"><?php the_sub_field('main_title'); ?></h3>
+		<?php endif; ?>
 		<div class="row">
 			<div class="col-lg-11 mx-auto">
-				<ul class="nav nav-tabs" id="myTab" role="tablist">
-					<li class="nav-item" role="presentation">
-						<a class="nav-link active" id="tab1-tab" data-bs-toggle="tab" data-bs-target="#tab1" role="tab" aria-controls="tab1" aria-selected="true">CSPM</a>
+
+				<?php if( have_rows('cloud_tab_repeater') ): ?>
+
+				<ul class="nav nav-tabs" role="tablist">
+
+					<?php
+					$i=1;
+					while( have_rows('cloud_tab_repeater') ): the_row();
+					?>
+
+					<?php
+					if($i==1) { ?>
+					<li class="nav-item">
+						<a id="tab<?php echo $i; ?>" href="#pane-<?php echo $i; ?>" class="nav-link active" data-bs-toggle="tab" role="tab">
+							<?php if(get_sub_field('tab_name')): ?>
+							<?php the_sub_field('tab_name'); ?>
+							<?php endif; ?>
+						</a>
 					</li>
-					<li class="nav-item" role="presentation">
-						<a class="nav-link " id="tab2-tab" data-bs-toggle="tab" data-bs-target="#tab2" role="tab" aria-controls="tab2" aria-selected="true">SSPM</a>
+					<?php
+							  } 
+
+					else { ?>
+					<li class="nav-item">
+						<a id="tab<?php echo $i; ?>" href="#pane-<?php echo $i; ?>" class="nav-link" data-bs-toggle="tab" role="tab">
+							<?php if(get_sub_field('tab_name')): ?>
+							<?php the_sub_field('tab_name'); ?>
+							<?php endif; ?>
+						</a>
 					</li>
+					<?php
+						 }
+					?>
+					<?php $i++;
+					endwhile;
+					?>
 				</ul>
+
 			</div>
 
 			<div class="col-lg-11 mx-auto cloud-tab-content">
 				<div id="content" class="tab-content" role="tablist">
-					<div class="card tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
-						<div class="card-header" role="tab" id="heading-1">
+
+					<?php
+					$i=1;
+					while( have_rows('cloud_tab_repeater') ): the_row();
+					?>
+					<?php
+					if($i==1) { ?>
+					<div class="card tab-pane fade show active" id="pane-<?php echo $i; ?>" role="tabpanel" aria-labelledby="tab<?php echo $i; ?>-tab">
+						<div class="card-header" role="tab" id="heading-<?php echo $i; ?>">
 							<h4 class="mb-0">
-								<a data-bs-toggle="collapse" href="#collapse-1" aria-expanded="true" aria-controls="collapse-1">CSPM</a>
-							</h4>
-						</div>
-						<div id="collapse-1" class="collapse show" data-bs-parent="#content" role="tabpanel" aria-labelledby="heading-1">
-							<div class="card-body">
-                <div class="row">
-                  <div class="col-lg-6 col-md-12 me-auto align-self-center order-sm-12">
-                    <img class="img-fluid tab-img" src="/wp-content/uploads/2022/12/CSPM.webp" alt="">
-                  </div>
-                  <div class="col-lg-5 col-md-12 me-auto align-self-center order-sm-12 tab-content-wrap">
-                    <h3 class="main-title v-subtitle-w">Cloud Security Posture Management</h3>
-                    <P class="tag-text">Gain a comprehensive view of risk posture across your cloud assets. Identify, analyze, and prioritize these risks with accuracy and agility.</P>
-                    <P class="tag-text-w">Supported applications</P>
-                    <div class="img-slider">
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/Aws-1.svg" class="slide-icon">
-                      </div>
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/Azzure.svg" class="slide-icon">
-                      </div>
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/google_cloud.svg" class="slide-icon">
-                      </div>
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/Linode.svg" class="slide-icon">
-                      </div>
-                    </div>
-                    <a  href="#" class="cta cta-r">
-                      <div data-hover="Know More" class="text-wrap">
-                        <div class="btn-link">
-                          <span class="link">Know More</span>
-                        </div> 
-                      </div> 
-                      <div class="icon-img">
-                        <span>
-                          <img src="/wp-content/uploads/2022/12/right-2.png" class="i-img">
-                        </span>
-                      </div>
-                    </a>
-                  </div>
+								<a data-bs-toggle="collapse" href="#collapse-<?php echo $i; ?>" aria-expanded="true" aria-controls="collapse-<?php echo $i; ?>">
+									<?php if(get_sub_field('tab_name')): ?>
+									<?php the_sub_field('tab_name'); ?>
+									<?php endif; ?></a>
+									</h4>
+								</div>
+							<div id="collapse-<?php echo $i; ?>" class="collapse show" data-bs-parent="#content" role="tabpanel" aria-labelledby="heading-<?php echo $i; ?>">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-lg-6 col-md-12 me-auto align-self-center order-sm-12">
+											<?php if(get_sub_field('tab_image')): ?>
+											<img class="img-fluid tab-img" src="<?php the_sub_field('tab_image'); ?>" alt="">
+											<?php endif; ?>
+										</div>
+										<div class="col-lg-5 col-md-12 me-auto align-self-center order-sm-12 tab-content-wrap">
+											<?php if(get_sub_field('tab_title')): ?>
+											<h3 class="main-title v-subtitle-w"><?php the_sub_field('tab_title'); ?></h3>
+											<?php endif; ?>
+											<?php if(get_sub_field('description')): ?>
+											<P class="tag-text"><?php the_sub_field('description'); ?></P>
+											<?php endif; ?>
+											<?php if(get_sub_field('slider_title')): ?>
+											<P class="tag-text-w"><?php the_sub_field('slider_title'); ?></P>
+											<?php endif; ?>
+
+											<div class="img-slider">
+												<?php if( have_rows('image_slider') ):
+							   while ( have_rows('image_slider') ) : the_row();?>
+												<div class="img-slide">
+													<img src="<?php the_sub_field('slide_image'); ?>" class="slide-icon">
+												</div>
+												<?php
+							   endwhile;
+							   else :
+							   endif;
+												?>
+											</div>
+											<?php if(get_sub_field('tab_cta_link')): ?>
+											<a  href="<?php the_sub_field('tab_cta_link'); ?>" class="cta cta-r">
+												<div data-hover="<?php the_sub_field('tab_cta'); ?>" class="text-wrap">
+													<div class="btn-link">
+														<span class="link"><?php the_sub_field('tab_cta'); ?></span>
+													</div> 
+												</div> 
+												<div class="icon-img">
+													<span>
+														<img src="/wp-content/uploads/2022/12/right-2.png" class="i-img">
+													</span>
+												</div>
+											</a>
+											<?php endif; ?>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="card tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
-						<div class="card-header" role="tab" id="heading-2">
-							<h4 class="mb-0">
-								<a data-bs-toggle="collapse" href="#collapse-2" aria-expanded="true" aria-controls="collapse-2">SSPM</a>
-							</h4>
-						</div>
-						<div id="collapse-2" class="collapse show" data-bs-parent="#content" role="tabpanel" aria-labelledby="heading-2">
-							<div class="card-body">
-                <div class="row">
-                  <div class="col-lg-6 col-md-12 me-auto align-self-center order-sm-12">
-                    <img class="img-fluid tab-img" src="/wp-content/uploads/2022/12/SSPM.webp" alt="">
-                  </div>
-                  <div class="col-lg-5 col-md-12 me-auto align-self-center order-sm-12 tab-content-wrap">
-                    <h3 class="main-title v-subtitle-w">SaaS security Posture Management</h3>
-                    <P class="tag-text">Shield your SaaS applications from unauthorized users and avoid data leakages. Get complete visibility, monitor access, and follow all compliance and security policies.</P>
-                    <P class="tag-text-w">Supported applications</P>
-                    <div class="img-slider">
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/Aws-1.svg" class="slide-icon">
-                      </div>
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/Azzure.svg" class="slide-icon">
-                      </div>
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/google_cloud.svg" class="slide-icon">
-                      </div>
-                      <div class="img-slide">
-                        <img src="/wp-content/uploads/2022/12/Linode.svg" class="slide-icon">
-                      </div>
-                    </div>
-                    <a  href="#" class="cta cta-r">
-                      <div data-hover="Know More" class="text-wrap">
-                        <div class="btn-link">
-                          <span class="link">Know More</span>
-                        </div> 
-                      </div> 
-                      <div class="icon-img">
-                        <span>
-                          <img src="/wp-content/uploads/2022/12/right-2.png" class="i-img">
-                        </span>
-                      </div>
-                    </a>
-                  </div>
+						<?php
+							  } 
+					else { ?>
+						<div class="card tab-pane fade" id="pane-<?php echo $i; ?>" role="tabpanel" aria-labelledby="tab<?php echo $i; ?>-tab">
+							<div class="card-header" role="tab" id="heading-<?php echo $i; ?>">
+								<h4 class="mb-0">
+									<a data-bs-toggle="collapse" href="#collapse-<?php echo $i; ?>" aria-expanded="true" aria-controls="collapse-<?php echo $i; ?>">
+										<?php if(get_sub_field('tab_name')): ?>
+										<?php the_sub_field('tab_name'); ?>
+										<?php endif; ?></a>
+								</h4>
+							</div>
+							<div id="collapse-<?php echo $i; ?>" class="collapse" data-bs-parent="#content" role="tabpanel" aria-labelledby="heading-<?php echo $i; ?>">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-lg-6 col-md-12 me-auto align-self-center order-sm-12">
+											<?php if(get_sub_field('tab_image')): ?>
+											<img class="img-fluid tab-img" src="<?php the_sub_field('tab_image'); ?>" alt="">
+											<?php endif; ?>
+										</div>
+										<div class="col-lg-5 col-md-12 me-auto align-self-center order-sm-12 tab-content-wrap">
+											<?php if(get_sub_field('tab_title')): ?>
+											<h3 class="main-title v-subtitle-w"><?php the_sub_field('tab_title'); ?></h3>
+											<?php endif; ?>
+											<?php if(get_sub_field('description')): ?>
+											<P class="tag-text"><?php the_sub_field('description'); ?></P>
+											<?php endif; ?>
+											<?php if(get_sub_field('slider_title')): ?>
+											<P class="tag-text-w"><?php the_sub_field('slider_title'); ?></P>
+											<?php endif; ?>
+
+											<div class="img-slider">
+												<?php if( have_rows('image_slider') ):
+						  while ( have_rows('image_slider') ) : the_row();?>
+												<div class="img-slide">
+													<img src="<?php the_sub_field('slide_image'); ?>" class="slide-icon">
+												</div>
+												<?php
+						  endwhile;
+						  else :
+						  endif;
+												?>
+											</div>
+											<?php if(get_sub_field('tab_cta_link')): ?>
+											<a  href="<?php the_sub_field('tab_cta_link'); ?>" class="cta cta-r">
+												<div data-hover="<?php the_sub_field('tab_cta'); ?>" class="text-wrap">
+													<div class="btn-link">
+														<span class="link"><?php the_sub_field('tab_cta'); ?></span>
+													</div> 
+												</div> 
+												<div class="icon-img">
+													<span>
+														<img src="/wp-content/uploads/2022/12/right-2.png" class="i-img">
+													</span>
+												</div>
+											</a>
+											<?php endif; ?>
+										</div>
+									</div>
 								</div>
 							</div>
+							<?php
+						 }
+							?>
+
+							<?php $i++;
+							endwhile;
+							?>
 						</div>
 					</div>
+
+					<?php  
+					endif; ?>
 				</div>
 			</div>
 		</div>
-	</div>
-</section> -->
+		</section>
+	<?php
+	endwhile;
+	else :
+	endif;
+	?>
+
+
 <?php if( have_rows('approach_wrap') ):
   while ( have_rows('approach_wrap') ) : the_row();?>
 <section class="c-approch">
@@ -291,7 +375,7 @@ endif;
       <div class="col-lg-11 mx-auto">
         <div class="row">
           <div class="col-lg-5 align-self-center">
-            <h3 class="v-title-wwr"><?php the_field('form_title');?></h3>
+            <h3 class="v-title-wwr1"><?php the_field('form_title');?></h3>
           </div>
           <div class="col-lg-7 m-pad">
             <?php the_field('home_form');?>
